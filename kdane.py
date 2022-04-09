609cf25aa4fc930e7d0cdd45dea6b89fbe95fbee
RahulRH07 <87711547+RahulRH07@users.noreply.github.com>
2022-04-10 01:21:09 +0530

    Update kdane.py

--- kdane.py
+++ kdane.py
@@ -1,15 +1,14 @@
-# Python program to find maximum contiguous subarray
+# Python program to find sum of maximum contiguous subarray
 
 def kadane(a,size):
+        max_sum = a[0]
+        cur_max = a[0]
 
-	max_sum =a[0]
-	cur_max = a[0]
+        for i in range(1,size):
+                cur_max = max(a[i], cur_max + a[i])
+                max_sum = max(max_sum,cur_max)
 
-	for i in range(1,size):
-		cur_max = max(a[i], cur_max + a[i])
-		max_sum = max(max_sum,cur_max)
-		
-		return max_sum
-
+        return max_sum
+        
 a = [1 , 2, -1, 3, 4, -2]
 print('Maximum contiguous sum is' , kadane(a,len(a)))
